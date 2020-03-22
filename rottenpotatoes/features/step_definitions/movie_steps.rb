f56cc0d12ae5400fd73ffd8c3ccda672f721c53f
Justin Zwick <jaz2130@columbia.edu>
2020-03-22 13:31:44 -0400

    complete part 3

--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -18,7 +18,8 @@ end
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.body is the entire content of the page as a string.
-  fail "Unimplemented"
+  movies_shown = page.all("#movies tbody td:nth-child(1)").map { |m| m.text }
+  (movies_shown.index(e1) < movies_shown.index(e2)).should be true
 end
 
 # Make it easier to express checking or unchecking several boxes at once
